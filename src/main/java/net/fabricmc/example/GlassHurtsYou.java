937630ccb6ce36a96092a14903dd9e891198ab12
Mangrove Modder <106411187+MangroveModder@users.noreply.github.com>
2022-05-27 19:33:03 -0400

    remove comments

--- src/main/java/net/fabricmc/example/GlassHurtsYou.java
+++ src/main/java/net/fabricmc/example/GlassHurtsYou.java
@@ -9,24 +9,16 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import java.util.Random;
-
 public class GlassHurtsYou implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		// Block break event
 		PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, entity) -> {
+			// Glass check
 			if (state.getMaterial() == Material.GLASS) {
 				Random random = new Random();
-				player.damage(DamageSource.GENERIC, random.nextInt(1, 3));
-				player.playSound(SoundEvents.ENTITY_SPLASH_POTION_BREAK, SoundCategory.BLOCKS, 1, random.nextFloat(1.0F, 1.3F));
+				player.damage(DamageSource.GENERIC, random.nextInt(1, 3));// Damage player
+				player.playSound(SoundEvents.ENTITY_SPLASH_POTION_BREAK, SoundCategory.BLOCKS, 1, random.nextFloat(1.0F, 1.3F));//Play sound for glass break
 			}
 		});
 	}
